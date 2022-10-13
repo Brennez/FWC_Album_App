83990069cb0c8248e2d38eac6ad465e4e352ae62
Brennez <tchalisantos40@gmail.com>
2022-10-13 16:16:52 -0300

    initializing connection with the backend

---
+++ lib/app/core/rest/custom_dio.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:dio/native_imp.dart';
+
+import '../config/env/env.dart';
+
+class CustomDio extends DioForNative {
+  CustomDio()
+      : super(BaseOptions(
+          baseUrl: Env.instance['backend_base_url'] ?? '',
+          connectTimeout: 5000,
+          receiveTimeout: 60000,
+        ));
+}
